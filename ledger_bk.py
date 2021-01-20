721d1ddbc0321bda6062764ea218a663f050daa6
Jiiihun <rjh7209@naver.com>
2021-01-21 04:10:02 +0900

    계좌 개설 부분 수정

--- ledger_bk.py
+++ ledger_bk.py
@@ -3,9 +3,10 @@ import sqlite3
 def create():
     con = sqlite3.connect("aledger.db")
     cur = con.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS account(id INTEGER PRIMARY KEY,name TEXT,user TEXT, password TEXT,category TEXT,cdate TEXT)")
+    cur.execute("CREATE TABLE IF NOT EXISTS account(account_num TEXT PRIMARY KEY,name TEXT, password TEXT)")
     con.commit()
     con.close()
+    
 def viewall():
     con = sqlite3.connect("aledger.db")
     cur = con.cursor()
@@ -14,120 +15,29 @@ def viewall():
     con.close()
     return rows
 
-def search(name="",user="",password="",category=""):
+# def search(name="",user="",password="",category=""):
+#     con = sqlite3.connect("aledger.db")
+#     cur = con.cursor()
+#     cur.execute("SELECT * FROM account WHERE name=? OR user=? OR password=? OR category=?",(name,user,password,category))
+#     rows = cur.fetchall()
+#     con.close()
+#     return rows
+def add(account_num, name, password):
     con = sqlite3.connect("aledger.db")
     cur = con.cursor()
-    cur.execute("SELECT * FROM account WHERE name=? OR user=? OR password=? OR category=?",(name,user,password,category))
-    rows = cur.fetchall()
-    con.close()
-    return rows
-def add(name,user,password,category,cdate):
-    con = sqlite3.connect("aledger.db")
-    cur = con.cursor()
-    cur.execute("INSERT INTO account VALUES(NULL,?,?,?,?,?)",(name,user,password,category,cdate))
+    cur.execute("INSERT INTO account VALUES(?,?,?)",(account_num, name,password))
     con.commit()
     con.close()
-def update(id,name,user,password,category,cdate):
+# def update(id,name,user,password,category,cdate):
+#     con = sqlite3.connect("aledger.db")
+#     cur = con.cursor()
+#     cur.execute("UPDATE account SET name=?,user=?,password=?,category=?,cdate=? WHERE id=?",(name,user,password,category,cdate,id))
+#     con.commit()
+#     con.close()
+def delete(account_num):
     con = sqlite3.connect("aledger.db")
     cur = con.cursor()
-    cur.execute("UPDATE account SET name=?,user=?,password=?,category=?,cdate=? WHERE id=?",(name,user,password,category,cdate,id))
-    con.commit()
-    con.close()
-def delete(id):
-    con = sqlite3.connect("aledger.db")
-    cur = con.cursor()
-    cur.execute("DELETE FROM account WHERE id=?",(id,))
-    con.commit()
-    con.close()
-create()
-#print(search(category="social"))
-=======
-import sqlite3
-def create():
-    con = sqlite3.connect("aledger.db")
-    cur = con.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS account(id INTEGER PRIMARY KEY,name TEXT,user TEXT, password TEXT,category TEXT,cdate TEXT)")
-    con.commit()
-    con.close()
-def viewall():
-    con = sqlite3.connect("aledger.db")
-    cur = con.cursor()
-    cur.execute("SELECT * FROM account")
-    rows = cur.fetchall()
-    con.close()
-    return rows
-
-def search(name="",user="",password="",category=""):
-    con = sqlite3.connect("aledger.db")
-    cur = con.cursor()
-    cur.execute("SELECT * FROM account WHERE name=? OR user=? OR password=? OR category=?",(name,user,password,category))
-    rows = cur.fetchall()
-    con.close()
-    return rows
-def add(name,user,password,category,cdate):
-    con = sqlite3.connect("aledger.db")
-    cur = con.cursor()
-    cur.execute("INSERT INTO account VALUES(NULL,?,?,?,?,?)",(name,user,password,category,cdate))
-    con.commit()
-    con.close()
-def update(id,name,user,password,category,cdate):
-    con = sqlite3.connect("aledger.db")
-    cur = con.cursor()
-    cur.execute("UPDATE account SET name=?,user=?,password=?,category=?,cdate=? WHERE id=?",(name,user,password,category,cdate,id))
-    con.commit()
-    con.close()
-def delete(id):
-    con = sqlite3.connect("aledger.db")
-    cur = con.cursor()
-    cur.execute("DELETE FROM account WHERE id=?",(id,))
-    con.commit()
-    con.close()
-create()
-#print(search(category="social"))
-=======
-import sqlite3
-def create(): #DB생성
-    con = sqlite3.connect("aledger.db")
-    cur = con.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS account(id INTEGER PRIMARY KEY,name TEXT,user TEXT, password TEXT,category TEXT,cdate TEXT)")
-    #INTEGER PRIMARY KEY를 계좌번호로
-    con.commit()
-    con.close()
-
-def viewall(): #DB에 저장된 모든 정보 불러옴
-    con = sqlite3.connect("aledger.db")
-    cur = con.cursor()
-    cur.execute("SELECT * FROM account")
-    rows = cur.fetchall() #모든 줄 출력
-    con.close()
-    return rows
-
-def search(name="",user="",password="",category=""): #원하는 정보를 찾음
-    con = sqlite3.connect("aledger.db")
-    cur = con.cursor()
-    cur.execute("SELECT * FROM account WHERE name=? OR user=? OR password=? OR category=?",(name,user,password,category))
-    rows = cur.fetchall() #찾아온 정보를 모두 출력
-    con.close()
-    return rows
-
-def add(name,user,password,category,cdate): #DB에 새로운 정보 입력
-    con = sqlite3.connect("aledger.db")
-    cur = con.cursor()
-    cur.execute("INSERT INTO account VALUES(NULL,?,?,?,?,?)",(name,user,password,category,cdate))
-    con.commit()
-    con.close()
-
-def update(id,name,user,password,category,cdate): #있는 정보를 업데이트함
-    con = sqlite3.connect("aledger.db")
-    cur = con.cursor()
-    cur.execute("UPDATE account SET name=?,user=?,password=?,category=?,cdate=? WHERE id=?",(name,user,password,category,cdate,id))
-    con.commit()
-    con.close()
-
-def delete(id): #DB에 있는 정보 중 원하는 정보 삭제
-    con = sqlite3.connect("aledger.db")
-    cur = con.cursor()
-    cur.execute("DELETE FROM account WHERE id=?",(id,))
+    cur.execute("DELETE FROM account WHERE account_num=?",(account_num,))
     con.commit()
     con.close()
 create()
