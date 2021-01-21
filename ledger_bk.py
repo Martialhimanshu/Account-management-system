8b271980cb5552af1417601a173347619e18ec97
choijeensol <hate1710@naver.com>
2021-01-21 14:32:22 +0900

    안쓰는 부분 삭제

--- ledger_bk.py
+++ ledger_bk.py
@@ -15,14 +15,6 @@ def viewall():
     con.close()
     return rows
 
-#def search(name="",user="",password="",category=""):
-#    con = sqlite3.connect("aledger.db")
-#    cur = con.cursor()
-#    cur.execute("SELECT * FROM account WHERE name=? OR user=? OR password=? OR category=?",(name,user,password,category))
-#    rows = cur.fetchall()
-#    con.close()
-#    return rows
-
 def search(account_num="",name="",password=""):
     con = sqlite3.connect("aledger.db")
     cur = con.cursor()
@@ -37,13 +29,6 @@ def add(account_num, name, password, money):
     cur.execute("INSERT INTO account VALUES(?,?,?,?)",(account_num, name, password, money))
     con.commit()
     con.close()
-    
-#def update(account,name,password,money):
-    #con = sqlite3.connect("aledger.db")
-    #cur = con.cursor()
-    #cur.execute("UPDATE account SET account_num=?, name=?,password=?,money=?",(account_num, name, password, money))
-    #con.commit()
-    #con.close()
 
 def update(name,account_num, money):
    con = sqlite3.connect("aledger.db")
