b7ff28d02e6b5dd9749b301e0d0321e5a39ac890
amy9356 <hemnihemzzi@gmail.com>
2021-01-21 12:51:35 +0900

    계좌이체 입력, 버튼 생성

--- ledger.py
+++ ledger.py
@@ -2,31 +2,64 @@
 # Copyright 2017 by Martial Himanshu.  All rights reserved.
 # Distributed under the MPL license.  See LICENSE.txt for details.
 
+import tkinter.messagebox as msgbox # 메세지박스를 msgbox라는 이름으로 가져옴
 from tkinter import * #tkinter밑에있는 모든 정보 가져옴
 import ledger_bk #ledger_bk 정보 가져옴
+import datetime # datetime 정보 가져옴
+import random # random 정보 가져옴
 window = Tk() #윈도우 창 생성
-window.title("Account Ledger") #윈도우 창 타이틀
+window.title("계좌 관리 프로그램") #윈도우 창 타이틀
 #window.geometry("너비 x 높이 + x좌표 + y좌표") 윈도우 창의 너비, 높이, 초기화면 크기 설정
 #window.resizable(상하(True or False), 좌우) 윈도우 창의 창 크기 조절 가능 여부 설정, True로 설정 시 윈도우 창 크기 조절 가능
+
+menu = Menu(window) #menu = Menu(윈도우 창) 윈도우 창에서 메뉴 사용
+
+def rand_accnum() : # 랜덤한 계좌번호를 문자열로 생성해서 리턴 하는 함수
+    accnum_list = []
+    acc_num = ""
+
+    for i in range(16) :
+        accnum_list.append(random.randint(0, 9))
+    accnum_list[6] = '-'
+    accnum_list[9] = '-'
+
+    for j in range(16):
+        acc_num += str(accnum_list[j])
+    return acc_num
 
 def view_command():
     #lb=Listbox(window,height=20,width=94) (111번 줄에 정의)
     lb.delete(0,END) #0항목부터 END까지 삭제
-    for row in ledger_bk.viewall(): #ledger_bk파일에 있는 viewall()함수 이용
+    for row in ledger_bk.viewall(name.get()): #ledger_bk파일에 있는 viewall()함수 이용
         lb.insert(END,row) #lb(리스트박스) 끝에서부터 하나씩 DB에 있는 정보 하나씩 기입
 
-def search_command():
-    lb.delete(0,END) #0항목부터 END까지 삭제
-    for row in ledger_bk.search(name=name.get(),user=user.get(),password=password.get(),category=category.get()):
-    #ledger_bk파일에 있는 search()함수 이용
-        lb.insert(END,row) #lb 끝에서부터 하나씩 DB에 있는 정보 기입
+# def search_command():
+#     lb.delete(0,END) #0항목부터 END까지 삭제
+#     for row in ledger_bk.search(name=name.get(),user=user.get(),password=password.get(),category=category.get()):
+#     #ledger_bk파일에 있는 search()함수 이용
+#         lb.insert(END,row) #lb 끝에서부터 하나씩 DB에 있는 정보 기입
 
 def add_command():
-    ledger_bk.add(name.get(),user.get(),password.get(),category.get(),cdate.get())
+    # 정보 입력 시 빈칸이 있으면 에러 발생하고 저장 x
+    if name.get() == "" or password.get() == "" or money.get() == "" :
+        msgbox.showerror("에러", "정보 칸을 모두 입력해주세요!")
+        return -1
+
+    acc_number = rand_accnum()
+    ledger_bk.add(acc_number, name.get(), password.get(), money.get())
     #ledger_bk파일에 있는 add()함수 이용
     lb.delete(0,END) #0항목부터 END까지 삭제
-    lb.insert(END,name.get(),user.get(),password.get(),category.get(),cdate.get())
-    #lb에 ledger_bk파일에 있는 add()함수에서 받아온 정보를 끝에서부터 하나씩 기입
+    lb.insert(END,"이름 : " + name.get(),"계좌번호 : " + acc_number, "패스워드 : " + "*" * len(password.get()),"계좌잔고 : " + money.get(), "계좌 개설 시간 : " + datetime.datetime.now().strftime('%Y년 %m월 %d일 %H시 %M분 %S초'))
+    #lb에 ledger_bk파일에 있는 add()함수에서 받아온 정보와 계좌번호, 그리고 계좌를 생성한 시간을 화면에 출력
+
+#def deposit(de_money):
+#    deposit_money = money.get() + de_money
+
+#def withdraw(wi_money):
+#    if (money.get() > wi_money):
+#        withdraw_money = money.get() - wi_money
+#    else:
+#        withdraw_money = money.get()
 
 def get_selected_row(event):
     try:
@@ -37,98 +70,119 @@ def get_selected_row(event):
         #Entry(윈도우 창, 파라미터, ...)
         e1.delete(0,END) #
         e1.insert(END,selected_tuple[1])
-        e2.delete(0,END)
-        e2.insert(END,selected_tuple[2])
+        #e2.delete(0,END)
+        #e2.insert(END,selected_tuple[2])
         e3.delete(0,END)
         e3.insert(END,selected_tuple[3])
         e4.delete(0,END)
         e4.insert(END,selected_tuple[4])
-        e5.delete(0,END)
-        e5.insert(END,selected_tuple[5])
+        # e5.delete(0,END)
+        # e5.insert(END,selected_tuple[5])
+        e6.delete(0,END)
+        e6.insert(END,selected_tuple[6])
+        e7.delete(0,END)
+        e7.insert(END,selected_tuple[7])
+
     except IndexError:
         pass
 
-def update_command():
-    ledger_bk.update(selected_tuple[0],name.get(),user.get(),password.get(),category.get(),cdate.get())
-    view_command()
+# def update_command():
+#      ledger_bk.update(selected_tuple[0],account_num.get(),name.get(),password.get(),money.get())
+#      view_command()
 
 def delete_command():
     ledger_bk.delete(selected_tuple[0])
     view_command()
     #lb.delete(END,get_selected_row.selected_tuple)
+
 def clear_command():
     lb.delete(0,END) #0항목부터 END까지 삭제
     e1.delete(0,END)
-    e2.delete(0,END)
+    # e2.delete(0,END)
     e3.delete(0,END)
     e4.delete(0,END)
-    e5.delete(0,END)
+    # e5.delete(0,END)
+    e6.delete(0,END)
+    e7.delete(0,END)
+
 
 # def remittance_command():
 #     lb.delete(0,END)
 
-l1 = Label(window,text="Name")
+l1 = Label(window,text="성명")
 l1.grid(row=0,column=0,columnspan=2)
-l2 = Label(window,text="Username/Email")
-l2.grid(row=1,column=0,columnspan=2)
-l3 = Label(window,text="Password")
+# l2 = Label(window,text="Username/Email")
+# l2.grid(row=1,column=0,columnspan=2)
+l3 = Label(window,text="비밀번호")
 l3.grid(row=2,column=0,columnspan=2)
-l4 = Label(window,text="Category")
+l4 = Label(window,text="잔고")
 l4.grid(row=3,column=0,columnspan=2)
-l5 = Label(window,text="Date")
-l5.grid(row=4,column=0,columnspan=2)
+# l5 = Label(window,text="Date")
+# l5.grid(row=4,column=0,columnspan=2)
+l6 = Label(window,text="start_account")
+l6.grid(row=5,column=0,columnspan=2)
+l7 = Label(window,text="target_account")
+l7.grid(row=6,column=0,columnspan=2)
+
 
 name=StringVar()
 e1 = Entry(window,textvariable=name,width=50)
 e1.grid(row=0,column=0,columnspan=10)
 
-user=StringVar()
-e2 = Entry(window,textvariable=user,width=50)
-e2.grid(row=1,column=0,columnspan=10)
+# user=StringVar()
+# e2 = Entry(window,textvariable=user,width=50)
+# e2.grid(row=1,column=0,columnspan=10)
 
 password=StringVar()
 e3 = Entry(window,textvariable=password,width=50)
 e3.grid(row=2,column=0,columnspan=10)
 
-category=StringVar()
-e4 = Entry(window,textvariable=category,width=50)
+money=StringVar()
+e4 = Entry(window,textvariable=money,width=50)
 e4.grid(row=3,column=0,columnspan=10)
 
-cdate=StringVar()
-e5 = Entry(window,textvariable=cdate,width=50)
-e5.grid(row=4,column=0,columnspan=10)
+# cdate=StringVar()
+# e5 = Entry(window,textvariable=cdate,width=50)
+# e5.grid(row=4,column=0,columnspan=10)
 
-b1 = Button(window,text="Add",width=12,command=add_command)
+start_account=StringVar()
+e6 = Entry(window,textvariable=start_account, width=50)
+e6.grid(row=5,column=0,columnspan=10)
+
+target_account=StringVar()
+e7 = Entry(window,textvariable=target_account,width=50)
+e7.grid(row=6,column=0,columnspan=10)
+
+b1 = Button(window,text="계좌생성",width=12,command=add_command)
 #Button(윈도우 창, 파라미터, ...)
-b1.grid(row=5,column=0)
+b1.grid(row=7,column=0)
 
-b2 = Button(window,text="Update",width=12,command=update_command)
-b2.grid(row=5,column=1)
+#b2 = Button(window,text="deposit",width=12,command=update_command)
+#b2.grid(row=5,column=1)
 
-b3 = Button(window,text="Search",width=12,command=search_command)
-b3.grid(row=5,column=2)
+#b3 = Button(window,text="withdraw",width=12,command=update_command)
+#b3.grid(row=5,column=2)
 
-b4 = Button(window,text="View All",width=12,command=view_command)
-b4.grid(row=5,column=3)
+b4 = Button(window,text="계좌보기",width=12,command=view_command)
+b4.grid(row=7,column=1)
 
-b5 = Button(window,text="Delete",width=12,command=delete_command)
-b5.grid(row=5,column=4)
+b5 = Button(window,text="계좌폐기",width=12,command=delete_command)
+b5.grid(row=7,column=2)
 
-b6 = Button(window,text="Cancel",width=12,command=window.destroy)
-b6.grid(row=5,column=5)
+b6 = Button(window,text="나가기",width=12,command=window.destroy)
+b6.grid(row=7,column=4)
 
-b7 = Button(window,text="Clear All",width=12,command=clear_command)
+b7 = Button(window,text="초기화",width=12,command=clear_command)
 b7.grid(row=0,column=5)
 
-# b8 = Button(windouw, text="계좌이체",width=12,command=remittance_command)
-# b8.grid(row)
-
+b8 = Button(window, text="계좌이체",width=12)#command=remittance_command)
+b8.grid(row=7,column=3)
 
 lb=Listbox(window,height=20,width=94)
-lb.grid(row=6,column=0,columnspan=6)
+lb.grid(row=8,column=0,columnspan=6)
 
 sb=Scrollbar(window)
-sb.grid(row=6,column=6,rowspan=6)
+sb.grid(row=8,column=6,rowspan=6)
 
 lb.configure(yscrollcommand=sb.set)
 sb.configure(command=lb.yview)
--- ledger_bk.py
+++ ledger_bk.py
@@ -3,48 +3,52 @@ import sqlite3
 def create():
     con = sqlite3.connect("aledger.db")
     cur = con.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS account(id INTEGER PRIMARY KEY,name TEXT,user TEXT, password TEXT,category TEXT,cdate TEXT)")
+    cur.execute("CREATE TABLE IF NOT EXISTS account(account_num TEXT PRIMARY KEY, name TEXT, password TEXT, money INTEGER)")
     con.commit()
     con.close()
-def viewall():
+  
+def viewall(name):
     con = sqlite3.connect("aledger.db")
     cur = con.cursor()
-    cur.execute("SELECT * FROM account")
+    cur.execute("SELECT * FROM account WHERE name={}".format(name))
     rows = cur.fetchall()
     con.close()
     return rows
 
-def search(name="",user="",password="",category=""):
+# def search(name="",user="",password="",category=""):
+#     con = sqlite3.connect("aledger.db")
+#     cur = con.cursor()
+#     cur.execute("SELECT * FROM account WHERE name=? OR user=? OR password=? OR category=?",(name,user,password,category))
+#     rows = cur.fetchall()
+#     con.close()
+#     return rows
+
+def add(account_num, name, password, money):
     con = sqlite3.connect("aledger.db")
     cur = con.cursor()
-    cur.execute("SELECT * FROM account WHERE name=? OR user=? OR password=? OR category=?",(name,user,password,category))
-    rows = cur.fetchall()
-    con.close()
-    return rows
-def add(name,user,password,category,cdate):
-    con = sqlite3.connect("aledger.db")
-    cur = con.cursor()
-    cur.execute("INSERT INTO account VALUES(NULL,?,?,?,?,?)",(name,user,password,category,cdate))
+    cur.execute("INSERT INTO account VALUES(?,?,?,?)",(account_num, name, password, money))
     con.commit()
     con.close()
-def update(id,name,user,password,category,cdate):
+    
+#  def update(account_num,name,password):
+#      con = sqlite3.connect("aledger.db")
+#      cur = con.cursor()
+#      cur.execute("UPDATE account SET account_num=?, name=?,password=?,money=?",(account_num,name,password,money))
+#      con.commit()
+#      con.close()
+
+def delete(account_num):
     con = sqlite3.connect("aledger.db")
     cur = con.cursor()
-    cur.execute("UPDATE account SET name=?,user=?,password=?,category=?,cdate=? WHERE id=?",(name,user,password,category,cdate,id))
+    cur.execute("DELETE FROM account WHERE account_num=?",(account_num,))
     con.commit()
     con.close()
-def delete(id):
+    
+def remittance(self,start_number, target_number, amount, password):
     con = sqlite3.connect("aledger.db")
     cur = con.cursor()
-    cur.execute("DELETE FROM account WHERE id=?",(id,))
+    cur.execute("REMETTANCE from account=?, to account=?, money=?, password=?",(start_number, target_number, amount, password))
     con.commit()
     con.close()
 create()
 #print(search(category="social"))
-
-# def remittance(self,start_number, target_number, amout, password):
-#     con = sqlite3.connect("aledger.db")
-#     cur = con.cursor()
-#     cur.execute("")
-#     con.commit()
-#     con.close()
